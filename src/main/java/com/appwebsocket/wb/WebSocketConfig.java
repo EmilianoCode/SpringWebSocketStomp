9874c0ab88ad390a362753319dc9b3255e918c96
EMILIANO2M <emiliano.montaldo@estudiantes.utec.edu.uy>
2023-02-08 18:56:47 -0300

    ..

--- src/main/java/com/appwebsocket/wb/WebSocketConfig.java
+++ src/main/java/com/appwebsocket/wb/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("*").withSockJS();
+		registry.addEndpoint("/gs-guide-websocket").setAllowedOriginPatterns("https://springwebsocketstomp-production.up.railway.app/").withSockJS();
 
 	}
 
