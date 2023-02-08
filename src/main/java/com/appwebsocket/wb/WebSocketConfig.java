84c98d5f35bfb2762d10a460653710ad6cf8e75e
EMILIANO2M <emiliano.montaldo@estudiantes.utec.edu.uy>
2023-02-08 18:39:58 -0300

    ..

--- src/main/java/com/appwebsocket/wb/WebSocketConfig.java
+++ src/main/java/com/appwebsocket/wb/WebSocketConfig.java
@@ -1,10 +1,16 @@
 package com.appwebsocket.wb;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -21,6 +27,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/gs-guide-websocket").withSockJS();
 
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource()
+	{
+		CorsConfiguration configuration = new CorsConfiguration(); /////
+		// configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://angular-springboot1-beta.vercel.app"));
+		configuration.setAllowedOriginPatterns(List.of("https://springwebsocketstomp-production.up.railway.app/"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+		configuration.setAllowedHeaders(List.of("Access-Control-Allow-Origin", "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
