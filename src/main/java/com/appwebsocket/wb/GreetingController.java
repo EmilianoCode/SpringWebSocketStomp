e66d4340bc3bd8c77be1326ae4eb352530b8fd60
EMILIANO2M <emiliano.montaldo@estudiantes.utec.edu.uy>
2023-02-08 17:28:16 -0300

    ..

--- src/main/java/com/appwebsocket/wb/GreetingController.java
+++ src/main/java/com/appwebsocket/wb/GreetingController.java
@@ -13,6 +13,11 @@ public class GreetingController {
 	@SendTo("/topic/greetings")
 	public Greeting greeting(HelloMessage message) throws Exception {
 		Thread.sleep(1000); // simulated delay
+		System.out.println("---------------------");
+		System.out.println(message.getName());
+		System.out.println(message.getName());
+		System.out.println(message.getName());
+		System.out.println("---------------------");
 		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
 	}
 
